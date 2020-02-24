a05c2e17a102b38ae24790203d20aaa061ff1d10
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-02-24 11:15:17 +0100

    Added content to Education.

--- lib/widgets/resume_page/sections/education.dart
+++ lib/widgets/resume_page/sections/education.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jlaskowska/widgets/resume_page/sections/common/headline.dart';
 
 class Education extends StatelessWidget {
   const Education({Key key}) : super(key: key);
@@ -6,8 +7,31 @@ class Education extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      color: Colors.green,
+      width: double.infinity,
+      color: Colors.grey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Headline('Education'),
+          SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'SWPS University',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  Text('Master of Science in Clinical Psychology'),
+                  Text('expected June 2021', style: TextStyle(fontSize: 10)),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
