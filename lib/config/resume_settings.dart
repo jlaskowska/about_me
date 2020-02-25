f1e99040a59135b998a01724912802fb9b6a87b0
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-02-25 16:12:18 +0100

    Added content to Projects.

--- lib/config/resume_settings.dart
+++ lib/config/resume_settings.dart
@@ -22,17 +22,33 @@ class ResumeSettings {
   static const List<ProjectDataModel> projects = const [
     ProjectDataModel(
       projectTitle: 'Website',
-      bulletPoints: [],
+      bulletPoints: [
+        'Responsive personal website and app.',
+        'This resume that you are reading :)',
+        'Punkt 1',
+        'Punkt 1',
+        'Punkt 1',
+      ],
       assetPath: 'assets/images/resume/jl_app_icon.png',
     ),
     ProjectDataModel(
       projectTitle: 'Todo',
-      bulletPoints: [],
+      bulletPoints: [
+        'App to keep track of one\'s todos.',
+        'Persists data to device using Hive.',
+        'Supports both light and dark mode.',
+        'Localized into three languages.',
+        'Animated chart of completed tasks.',
+      ],
       assetPath: 'assets/images/resume/todo_app_icon.png',
     ),
     ProjectDataModel(
       projectTitle: 'Signup',
-      bulletPoints: [],
+      bulletPoints: [
+        'App to sign-up, sign-in and sign-out.',
+        'Main functionality defined in interface, implementation powered by Firebase.',
+        'Unique background design achieved using custom drawn shapes & curves.',
+      ],
       assetPath: 'assets/images/resume/signup_app_icon.png',
     )
   ];
--- lib/widgets/resume_page/sections/projects.dart
+++ lib/widgets/resume_page/sections/projects.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:jlaskowska/config/project_colors.dart';
+import 'package:jlaskowska/config/resume_settings.dart';
+import 'package:jlaskowska/models/project_data_model.dart';
+import 'package:jlaskowska/widgets/resume_page/sections/common/headline.dart';
 
 class Projects extends StatelessWidget {
   const Projects({Key key}) : super(key: key);
@@ -6,29 +10,90 @@ class Projects extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 600,
-      color: Colors.blue.shade100,
+      color: Colors.grey,
       width: double.infinity,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Expanded(
-            child: Container(
-              // height: 100,
-              color: Colors.blue.shade400,
+          Headline('Projects'),
+          for (ProjectDataModel project in ResumeSettings.projects)
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: _ProjectCard(
+                title: project.projectTitle,
+                bulletPoints: project.bulletPoints,
+                assetPath: project.assetPath,
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class _ProjectCard extends StatelessWidget {
+  final String title;
+  final List<String> bulletPoints;
+  final String assetPath;
+
+  const _ProjectCard({
+    @required this.title,
+    @required this.bulletPoints,
+    @required this.assetPath,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(0),
+      color: ProjectColors.darkGray,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: ProjectColors.lightBlack,
+            ),
+            child: ListTile(
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.asset(
+                  assetPath,
+                  width: 40,
+                  height: 40,
+                ),
+              ),
+              title: Text(title),
             ),
           ),
-          SizedBox(height: 8),
-          Expanded(
-            child: Container(
-              // height: 100,
-              color: Colors.blue.shade600,
-            ),
-          ),
-          SizedBox(height: 8),
-          Expanded(
-            child: Container(
-              // height: 100,
-              color: Colors.blue.shade800,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 4),
+                for (String bulletPoint in bulletPoints)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Container(
+                      padding: const EdgeInsets.all(4.0),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: ProjectColors.lightBlack,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Text(
+                        bulletPoint,
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ),
+                SizedBox(height: 4),
+              ],
             ),
           ),
         ],
