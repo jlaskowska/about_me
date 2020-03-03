0800eb25ec79a10018d69767c74ee220020fad36
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-03 17:41:21 +0100

    Updated _ProjectSheet to be responsive across platforms.

--- lib/widgets/portfolio_page/portfolio_page.dart
+++ lib/widgets/portfolio_page/portfolio_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:about_me/config/portfolio_settings.dart';
 import 'package:about_me/config/project_colors.dart';
 import 'package:about_me/models/portfolio_projects_model.dart';
@@ -102,6 +104,11 @@ class _PortfolioTile extends StatelessWidget {
 }
 
 class _ProjectSheet extends StatelessWidget {
+  static const _padding = 32.0;
+  static const _titleFontSize = 48;
+  static const _desciptionTextWidth = 400.0;
+  static const _imageWidth = 300.0;
+
   final String title;
   final String description;
   final List<String> imageAssetPaths;
@@ -115,70 +122,98 @@ class _ProjectSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(32.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.arrow_back,
-              color: Theme.of(context).accentColor,
-            ),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  SizedBox(height: 48),
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.bold,
-                      color: ProjectColors.lightBlack,
+    return LayoutBuilder(
+      builder: (_, constraints) {
+        // determine number of images to show
+        int numberOfImagesToShow = 0;
+        if (constraints.maxWidth >= 1450) {
+          numberOfImagesToShow = 3;
+        } else if (constraints.maxWidth >= 1080) {
+          numberOfImagesToShow = 2;
+        } else if (constraints.maxWidth >= 780) {
+          numberOfImagesToShow = 1;
+        }
+
+        // determine available width
+        final availableWidth = constraints.maxWidth - 2 * _padding;
+
+        /// determine desciptionTextWidth which is the smaller value of availableWidth and constant _desciptionTextWidth
+        final desciptionTextWidth = min(availableWidth, _desciptionTextWidth);
+
+        /// determine a scale factor to scale title font size. this is at most 1.
+        final scale = min(availableWidth / _desciptionTextWidth, 1);
+        final titleFontSize = scale * _titleFontSize;
+
+        return SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(_padding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                if (kIsWeb)
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Theme.of(context).accentColor,
                     ),
+                    onPressed: () => Navigator.of(context).pop(),
                   ),
-                  SizedBox(height: 32),
-                  Container(
-                    width: 400,
-                    child: Text(
-                      description,
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: ProjectColors.lightBlack,
-                      ),
-                      maxLines: 50,
-                    ),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: Row(
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    if (imageAssetPaths.length == 0)
-                      for (final _ in [0, 1, 2]) Placeholder(fallbackHeight: 650, fallbackWidth: 300),
-                    if (imageAssetPaths.length != 0)
-                      for (String assetPath in imageAssetPaths)
-                        Image.asset(
-                          assetPath,
-                          width: 300,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        if (kIsWeb) SizedBox(height: 48),
+                        Text(
+                          title,
+                          style: TextStyle(
+                            // fontSize: 48,
+                            fontSize: titleFontSize,
+                            fontWeight: FontWeight.bold,
+                            color: ProjectColors.lightBlack,
+                          ),
                         ),
+                        SizedBox(height: 32),
+                        Container(
+                          width: desciptionTextWidth,
+                          child: Text(
+                            description,
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: ProjectColors.lightBlack,
+                            ),
+                            maxLines: 50,
+                          ),
+                        ),
+                      ],
+                    ),
+                    if (numberOfImagesToShow > 0)
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            for (int i = 0; i < numberOfImagesToShow; i++)
+                              imageAssetPaths.length > i
+                                  ? Image.asset(
+                                      imageAssetPaths[i],
+                                      width: _imageWidth,
+                                    )
+                                  : Placeholder(fallbackHeight: 650, fallbackWidth: _imageWidth)
+                          ],
+                        ),
+                      ),
                   ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
