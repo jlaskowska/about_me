2fd6950be61f817e15a9843e563b6a88986e8a7e
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-04 12:11:23 +0100

    Added screenshots to portfolio.

--- lib/config/portfolio_settings.dart
+++ lib/config/portfolio_settings.dart
@@ -28,28 +28,44 @@ class PortfolioSettings {
       description:
           'An application which allows users to sign-up, sign-in and sign-out. The main functionality is defined by an interface, with a concrete implementation powered by Firebase. The unique background design was achieved by using CustomPainter to draw shapes and curves. Provider is used for dependency injection, while MobX is used as the project\'s state management solution.',
       iconAssetPath: 'assets/images/common/signup_app_icon.png',
-      screenshotAssetPaths: [],
+      screenshotAssetPaths: [
+        'assets/images/portfolio/signup/02.png',
+        'assets/images/portfolio/signup/02.png',
+        'assets/images/portfolio/signup/03.png',
+      ],
     ),
     PortfolioProjectsModel(
       title: 'Weather',
       description:
           'An application which allows the user to get the current weather (and five-day forecast) for their current location. The user has the option of using either degree Celsius or degree Fahrenheit, while if an error occurs (no internet, location permission not granted), a popup is displayed. Provider is used as the project\'s state management solution, while the forecast is taken from MetaWeather\'s API.',
       iconAssetPath: 'assets/images/common/weather_app_icon.png',
-      screenshotAssetPaths: [],
+      screenshotAssetPaths: [
+        'assets/images/portfolio/weather/01.gif',
+        'assets/images/portfolio/weather/02.png',
+        'assets/images/portfolio/weather/03.png',
+      ],
     ),
     PortfolioProjectsModel(
       title: 'Quiz',
       description:
           'A fun quiz application where the user is presented a series of questions and must answer either true or false. Each game consists of ten questions (taken from opentdb.com\'s API), with the result displayed afterwards. setState is used as the project\'s state management solution.',
       iconAssetPath: 'assets/images/common/quiz_app_icon.png',
-      screenshotAssetPaths: [],
+      screenshotAssetPaths: [
+        'assets/images/portfolio/quiz/02.png',
+        'assets/images/portfolio/quiz/02.png',
+        'assets/images/portfolio/quiz/03.png',
+      ],
     ),
     PortfolioProjectsModel(
       title: 'Questionnaires',
       description:
           'An application which allows the user to fill-out psychological questionnaires. For this first version, the questionnaire Satisfaction with Life Scale was chosen. The code architecture is designed so that a new questionnaire can be incorporated into the app by simply adding an additional json file. setState is used as the project\'s state management solution.',
       iconAssetPath: 'assets/images/common/questionnaires_app_icon.png',
-      screenshotAssetPaths: [],
+      screenshotAssetPaths: [
+        'assets/images/portfolio/questionnaires/02.png',
+        'assets/images/portfolio/questionnaires/02.png',
+        'assets/images/portfolio/questionnaires/03.png',
+      ],
     ),
   ];
 }
