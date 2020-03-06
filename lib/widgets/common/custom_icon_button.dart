e2edb45c3c9e2c8c92c01cefa5674bb0b3716107
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-06 18:25:35 +0100

    Added color to CustomIconButton.

--- lib/widgets/common/custom_icon_button.dart
+++ lib/widgets/common/custom_icon_button.dart
@@ -5,16 +5,21 @@ import 'package:flutter/material.dart';
 class CustomIconButton extends StatelessWidget {
   final IconData icon;
   final void Function() onPressed;
+  final Color color;
 
   const CustomIconButton({
     @required this.icon,
     @required this.onPressed,
+    this.color = Colors.white,
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final iconWidget = Icon(icon);
+    final iconWidget = Icon(
+      icon,
+      color: color,
+    );
 
     return kIsWeb
         ? AdaptiveButton(
